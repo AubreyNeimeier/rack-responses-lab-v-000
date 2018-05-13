15416e2ad053d987edc79e657f44c8f449d7602b
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-13 09:11:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     if Time.now.hour < 12
       resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon!"
+      resp.write "Good Afernoon!"
     end
 
     resp.finish
